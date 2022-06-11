96e2a178c59d5be435dec0a12d45f40662d87b2b
ChoongilLee <98940580+ChoongilLee@users.noreply.github.com>
2022-06-11 23:27:28 +0900

    dd

--- 문자열 압축/S2.py
+++ 문자열 압축/S2.py
@@ -1,24 +1,29 @@
 
-from tkinter import N
-
-
 def solution(s):
     length_s = len(s)
+    answer = length_s
     for i in range(1, length_s//2+1):
         n= i
-        list_a = [0]
         k = 0
+        h = 0
+        t = 0
         while n < length_s-i+1:
             if s[n:n+i] == s[n-i:n]:
-                list_a[k] += 1
+                h += 1
                 n += i
+                k = 1
             else:
-                n+=1
-                k+=1
-                list_a += [0]
-    print(list_a)
+                t += k
+                n+=i
+                k=0
+        t += k
+        ans = length_s -i*h + t
+        print(h,t)
+        if answer >= ans:
+            answer = ans
+    return answer
 
 test = input()
-solution(test)
+print(solution(test))
 
 
