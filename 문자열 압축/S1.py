0173182b1d2b21c7025e7c61d0a61991d757919e
ChoongilLee <98940580+ChoongilLee@users.noreply.github.com>
2022-06-13 18:28:08 +0900

    최종

--- 문자열 압축/S1.py
+++ 문자열 압축/S1.py
@@ -6,18 +6,23 @@ def solution(s):
         n= i
         k = 0
         h = 0
-        t = 0
+        t1 = 0
+        t2 = 0
         while n < length_s-i+1:
             if s[n:n+i] == s[n-i:n]:
                 h += 1
-                k = 1
+            elif h>0:
+                t1 += len(str(h))
+                t2 += h
+                h=0
             else:
-                t += k
-                k=0
+                continue
             n += i
-        t += k
-        ans = length_s -i*h + t
-        print(i,h,t)
+        if h:
+            t1 += len(str(h+1))
+            t2 += h
+        ans = length_s -i*t2 + t1
+        print(i,h,t1,t2)
         if answer > ans:
             answer = ans
         i +=1
