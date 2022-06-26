477332da74299528823eec2b82b9638753adc521
ChoongilLee <98940580+ChoongilLee@users.noreply.github.com>
2022-06-26 17:47:02 +0900

    d

--- 신고결과받기/S1.py
+++ 신고결과받기/S1.py
@@ -1,7 +1,7 @@
 
 id_list = ["muzi", "frodo", "apeach", "neo"]
 report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
-
+k=2
 def solution(id_list, report, k):
     answer = []
     dict_a = dict() #피신고자 : 신고자
